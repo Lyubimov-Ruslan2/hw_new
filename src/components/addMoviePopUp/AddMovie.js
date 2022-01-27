d6245a0a9c13137d07572aad2b13d236cd5c7a44
Ruslan Lyubimov <ruslan_lyubimov@epam.com>
2022-01-27 17:07:44 +0500

    finish popup

--- src/components/addMoviePopUp/AddMovie.js
+++ src/components/addMoviePopUp/AddMovie.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./AddMovie.styles.css";
+import arrow from "../../img/Arrowforgenre.svg";
 
 class AddMovie extends React.Component {
   render() {
@@ -27,17 +28,40 @@ class AddMovie extends React.Component {
                 />
               </div>
               <div>
-                <label htmlFor="form-movie-url">Movie URL</label>
-                <input
-                  className="form-movie-url"
-                  type="text"
-                  placeholder="https://"
-                />{" "}
-              </div>
-              <div>
                 <label htmlFor="form-rating">Rating</label>
                 <input className="form-rating" type="number" min="0" max="10" />
               </div>
+              <div>
+                <label htmlFor="form-genre">Genre</label>
+                <div className="custom-select">
+                  <select>
+                    <option value="crime">Crime</option>
+                    <option value="documentary">Documentary</option>
+                    <option value="horror">Horror</option>
+                    <option value="comedy">Comedy</option>
+                  </select>
+                  <img className="arrow-genre" src={arrow} alt="arrow" />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="form-movie-runtime">Runtime</label>
+                <input
+                  className="form-movie-runtime"
+                  type="text"
+                  placeholder="minutes"
+                />
+              </div>
+              <div>
+                <label htmlFor="form-movie-overview">OVERVIEW</label>
+                <textarea
+                  className="form-movie-overview"
+                  placeholder="Movie description"
+                />
+              </div>
+            </div>
+            <div className="btns-for-add">
+              <button className="btns-1">RESET</button>
+              <button className="btns-2">SUBMIT</button>
             </div>
           </div>
         </div>
